af77fafc4381c0302fe41a0bbcc3ed00a279653e
Vlad Nemirovskii <v.nemirovskii@viseven.com>
2022-05-23 13:25:03 +0300

    Updates

---
+++ precache-manifest.f03bfcba6e9b919fb3669360f959b678.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d732eec89c3bf2c4470bef40cbe695ee",
+    "url": "/pizza-react-redux/index.html"
+  },
+  {
+    "revision": "3b7a8176f5fe939e4c6e",
+    "url": "/pizza-react-redux/static/css/main.410824a1.chunk.css"
+  },
+  {
+    "revision": "2758d65d0f5167d15205",
+    "url": "/pizza-react-redux/static/js/2.98cfc797.chunk.js"
+  },
+  {
+    "revision": "4cf5c6c6f464cc15458a86cbdc669a15",
+    "url": "/pizza-react-redux/static/js/2.98cfc797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b7a8176f5fe939e4c6e",
+    "url": "/pizza-react-redux/static/js/main.91f7497a.chunk.js"
+  },
+  {
+    "revision": "3dfe7b6c6e99fb4f35a0",
+    "url": "/pizza-react-redux/static/js/runtime-main.1731e164.js"
+  },
+  {
+    "revision": "4325ed848b66ed370420ea7fea09bc80",
+    "url": "/pizza-react-redux/static/media/ProximaNova-Black.4325ed84.eot"
+  },
+  {
+    "revision": "a0606c5fef28b9e57f501db027063141",
+    "url": "/pizza-react-redux/static/media/ProximaNova-Black.a0606c5f.ttf"
+  },
+  {
+    "revision": "b978d1b46a86df7a012cf8442936a1cb",
+    "url": "/pizza-react-redux/static/media/ProximaNova-Black.b978d1b4.woff"
+  },
+  {
+    "revision": "10141a9d737da9c84e6e14ec5a562c6c",
+    "url": "/pizza-react-redux/static/media/ProximaNova-Bold.10141a9d.eot"
+  },
+  {
+    "revision": "926a08fb27e3303c7452b0bdd2d5e5ab",
+    "url": "/pizza-react-redux/static/media/ProximaNova-Bold.926a08fb.ttf"
+  },
+  {
+    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
+    "url": "/pizza-react-redux/static/media/ProximaNova-Bold.e2cf3dc2.woff"
+  },
+  {
+    "revision": "169bc0f1eb2d8d0d8678014d368b0226",
+    "url": "/pizza-react-redux/static/media/ProximaNova-Extrabld.169bc0f1.eot"
+  },
+  {
+    "revision": "51563919fd1af7376fac14b1194a8e4e",
+    "url": "/pizza-react-redux/static/media/ProximaNova-Extrabld.51563919.ttf"
+  },
+  {
+    "revision": "ccf59c77109aecc8deeeede82e3bac87",
+    "url": "/pizza-react-redux/static/media/ProximaNova-Extrabld.ccf59c77.woff"
+  },
+  {
+    "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
+    "url": "/pizza-react-redux/static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "7ce6760d17685c466ba04d1b2c63c38b",
+    "url": "/pizza-react-redux/static/media/ProximaNova-Regular.7ce6760d.ttf"
+  },
+  {
+    "revision": "b7c512788e3c77b0196f0bace8a88418",
+    "url": "/pizza-react-redux/static/media/ProximaNova-Regular.b7c51278.eot"
+  },
+  {
+    "revision": "ceb2faf089a96bede51ccde3693d012b",
+    "url": "/pizza-react-redux/static/media/empty-cart.ceb2faf0.png"
+  },
+  {
+    "revision": "f3327011e80a4caf08837bd844e21996",
+    "url": "/pizza-react-redux/static/media/pizza-logo.f3327011.svg"
+  }
+]);
